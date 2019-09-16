5ad6601866858d4b406722073f956d4530811f5d
josh hart <joshua.hart@protonmail.com>
2019-09-16 08:33:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,33 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    if combination.all do |index|
+        index == "X" || index == "O"
+        end
+      print combination
+    end
+  end
+end
+
+
+
+
+
+      print combination
+
+  end
+  return Status
+end
